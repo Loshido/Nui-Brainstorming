ee06514025e75cc45184536c9afd3a037935e57a
loshido <loshido@pm.me>
2025-01-31 22:12:35 +0100

    Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,31 @@
+import { component$ } from "@builder.io/qwik";
+import { Link } from "@builder.io/qwik-city";
+
+interface Props {
+    connecte: boolean
+}
+
+import Profil from "~/assets/logo.png?jsx" // ?jsx traite l'image comme un <img src="..." ...>
+
+import "./style.css" // On ajoute le style à la page avec vite (le truc qui fait fonctionner Qwik)
+export default component$(({ connecte }: Props) => {
+    return <header id="tete">
+        <Profil/> {/* Le logo du site */}
+        <nav>
+            <Link>
+                Accueil
+            </Link>
+            <Link>
+                Planning
+            </Link>
+            <Link>
+                Classements
+            </Link>
+            {
+                connecte 
+                ? <Link>Profil</Link>
+                : <Link>Se connecter</Link>
+            }
+        </nav>
+    </header>
+})
