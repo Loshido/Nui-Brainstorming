0db2b1d7bc4968137ad688fa4e198ea9c52d6651
loshido <loshido@pm.me>
2025-01-31 20:10:51 +0100

    First

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,24 +2,20 @@ import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
-  return (
-    <>
-      <h1>Hi 👋</h1>
-      <div>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </div>
+    return <>
+        <h1>NUIT de l'isen</h1>
+        <div>
+            1ère heure
+        </div>
     </>
-  );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
-  meta: [
-    {
-      name: "description",
-      content: "Qwik site description",
-    },
-  ],
+    title: "Welcome to Qwik",
+    meta: [
+        {
+            name: "description",
+            content: "Qwik site description",
+        },
+    ],
 };
